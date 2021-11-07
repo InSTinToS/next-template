fbee34c5d2d0e74c655617ec459226ae7e4b4df8
Miguel <miguelandradebarreto2@gmail.com>
2021-11-07 20:35:53 -0300

    :pencil::arrow_up: change packages and update packages

--- frontend/src/pages/users/[[...id]].tsx
+++ frontend/src/pages/users/[[...id]].tsx
@@ -3,19 +3,19 @@ import api from 'services/api'
 
 import Home from '../../pageComponents/Home'
 
-//? ISR
-export const getStaticPaths: GetStaticPaths = async () => ({
-  fallback: 'blocking',
-  paths: [{ params: { id: ['1'] } }, { params: { id: ['2'] } }]
-})
+//? ISR (not work)
+// export const getStaticPaths: GetStaticPaths = async () => ({
+//   fallback: 'blocking',
+//   paths: [{ params: { id: ['1'] } }, { params: { id: ['2'] } }]
+// })
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const res = await api.get(`users/${params.id}`)
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   const res = await api.get(`users/${params.id}`)
 
-  return {
-    revalidate: 60,
-    props: { message: res.data.message, id: res.data.id }
-  }
-}
+//   return {
+//     revalidate: 60,
+//     props: { message: res.data.message, id: res.data.id }
+//   }
+// }
 
 export default Home
